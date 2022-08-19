5e192b0dcdb76d0a05423a93481a335d3a064089
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2022-08-19 23:18:17 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 document.getElementById('btn').addEventListener('click', () => {
-    let str = document.getElementById('textarea').value
+    let str = document.getElementById('textarea').value;
 
     let sayHi = new Function(str);
 
